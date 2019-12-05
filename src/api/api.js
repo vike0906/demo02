d32351b5e0fe1c50669bbcf330edf850549c4ddc
liushengli <liujialong886@outlook.com>
2019-12-05 15:36:04 +0800

    -s

--- src/api/api.js
+++ src/api/api.js
@@ -5,10 +5,7 @@ import qs from 'qs';
 export const login = params => { return request({url: '/login',  method: 'post', data: qs.stringify(params) }); }
 
 //登出
-export const logout = () => { return request({url: '/logout',  method: 'get' }); }
-
-//获取菜单
-export const gainMenu = () => { return request({url: '/gainMenu',  method: 'get' }); }
+export const logout = () => { return request({url: '/logout',  method: 'post' }); }
 
 //修改密码
 export const changePassword = params => { return request({url: '/change-psd',  method: 'post' , data: qs.stringify(params)}); }
