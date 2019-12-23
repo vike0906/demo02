e8c0b68a6e7ee23ac73187e1bbd48fae57a23817
vike0906 <liujialong688@outlook.com>
2019-12-24 01:22:05 +0800

    ws

--- src/components/game/Game.vue
+++ src/components/game/Game.vue
@@ -1,21 +1,42 @@
 <template>
   <div class="warpper">
-    <canvas id="gobang" class="gobang" width="900" height="900" @click="on_click">
-      您的浏览器不支持 HTML5 canvas
-      标签
-    </canvas>
+    <div class="game-model" v-show="gameModelShow">
+      <a-button type="primary" class="model-button" block @click="palyerModel">玩家对战</a-button>
+      <br />
+      <br />
+      <br />
+      <a-button type="primary" class="model-button" block @click="computerModel">人机对战</a-button>
+    </div>
+    <div v-show="chessBoardShow">
+      <div>
+        <a-button type="primary" icon="double-left" @click="exitGame">退出对战</a-button>
+        <a-button type="primary" icon="reload" @click="refreshGame">重新开始</a-button>
+        <a-button type="primary" icon="like" @click="giveUp">认输</a-button>
+      </div>
+      <br />
+      <canvas id="gobang" class="gobang" width="900" height="900" @click="on_click">
+        您的浏览器不支持 HTML5 canvas
+        标签
+      </canvas>
+    </div>
     <!-- onmousemove="on_mouse_move(event)" -->
   </div>
 </template>
 
 <script>
+import { Modal } from "ant-design-vue";
 export default {
   data() {
     return {
+      gameModelShow: true,
+      chessBoardShow: false,
       gobang: {},
       cxt: {},
       chessBoard: [],
-      isFirst: true
+      isFirst: true,
+      isEnd: false,
+      path: "ws://localhost:8085/webSocket/chess/",
+      socket: null,
     };
   },
   mounted() {
@@ -30,8 +51,75 @@ export default {
       this.cxt = this.gobang.getContext("2d");
       this.fillArray();
       this.drawChessBoard();
+      this.createWebSocekt();
     },
 
+    createWebSocekt() {
+      if (typeof WebSocket === "undefined") {
+        alert("您的浏览器不支持socket");
+      } else {
+        // 实例化socket
+        let webSocketPath =
+          this.path + JSON.parse(sessionStorage.getItem("user")).token;
+        this.socket = new WebSocket(webSocketPath);
+        // 监听socket连接
+        this.socket.onopen = this.open;
+        // 监听socket错误信息
+        this.socket.onerror = this.error;
+        // 监听socket消息
+        this.socket.onmessage = this.getMessage;
+        // 销毁监听
+        this.socket.onclose = this.close;
+      }
+    },
+
+    open: function() {
+      console.log("socket连接成功");
+    },
+    error: function() {
+      console.log("连接错误");
+    },
+    getMessage: function(msg) {
+      console.log(msg.data);
+    },
+    send: function() {
+      this.socket.send(params);
+    },
+    close: function() {
+      console.log("socket已经关闭");
+    },
+
+    computerModel() {
+      Modal.info({
+        title: "暂未开通",
+        content: "人机对战模式暂未开通",
+        okText: "确定",
+        onOk() {
+          // pass
+        }
+      });
+    },
+    palyerModel() {
+      this.gameModelShow = false;
+      this.chessBoardShow = true;
+    },
+    exitGame() {
+      this.gameModelShow = true;
+      this.chessBoardShow = false;
+      this.endGame();
+    },
+    refreshGame() {
+      this.endGame();
+    },
+    giveUp() {
+      //pass
+    },
+    endGame() {
+      this.fillArray();
+      this.drawChessBoard();
+      this.isEnd = false;
+      this.isFirst = true;
+    },
     fillArray() {
       let chessBoard = new Array(15);
       for (let i = 0; i < 15; i++) {
@@ -77,26 +165,30 @@ export default {
         if (this.distance(ox, oy, chess)) {
           return;
         }
+        if (this.isEnd == true) {
+          this.$notification.open({
+            message: "对局已结束",
+            description: "本轮对局已结束，快点击重新开始开启下一轮对局吧",
+            icon: <a-icon type="smile" style="color: #108ee9" />
+          });
+          return;
+        }
+        this.socket.send(JSON.stringify({"x":x,"y":y}));
         chess.status = 1;
         this.onStep(chess.offsetX, chess.offsetY, this.isFirst);
-        let that = this;
-        setTimeout(function() {
-          if (that.isWin(x, y)) {
-            let msg = "白棋获胜";
-            if (that.isFirst) {
-              msg = "黑棋获胜";
-            }
-            // that.$message.info(message);
-            that.$notification.open({
-              message: msg,
-              description:
-                "五子连成一条直线即可获胜",
-              icon: <a-icon type="smile" style="color: #108ee9" />
-            });
-            that.fillArray();
-            that.drawChessBoard();
+        if (this.isWin(x, y)) {
+          this.isEnd = true;
+          let msg = "白棋获胜";
+          if (this.isFirst) {
+            msg = "黑棋获胜";
           }
-        }, 200);
+          // that.$message.info(message);
+          this.$notification.open({
+            message: msg,
+            description: "五子连成一条直线即可获胜",
+            icon: <a-icon type="smile" style="color: #108ee9" />
+          });
+        }
 
         this.isFirst = this.isFirst == true ? false : true;
       }
@@ -131,16 +223,16 @@ export default {
     },
 
     isWin(x, y) {
-      if (this.cross(x, y) == 5) {
+      if (this.cross(x, y) >= 5) {
         return true;
       }
-      if (this.vertical(x, y) == 5) {
+      if (this.vertical(x, y) >= 5) {
         return true;
       }
-      if (this.leftOblique(x, y) == 5) {
+      if (this.leftOblique(x, y) >= 5) {
         return true;
       }
-      if (this.rightOblique(x, y) == 5) {
+      if (this.rightOblique(x, y) >= 5) {
         return true;
       }
       return false;
@@ -218,7 +310,7 @@ export default {
         }
       }
       for (let i = 1; i < 5; i++) {
-        if (x + i <= 14 && y - i >= 0 && chessBoard[x + 1][y - i].status == 1) {
+        if (x + i <= 14 && y - i >= 0 && chessBoard[x + i][y - i].status == 1) {
           count++;
         } else {
           break;
@@ -234,6 +326,9 @@ export default {
       cxt.closePath();
       cxt.fill();
     }
+  },
+  destroyed() {
+    this.socket.close();
   }
 };
 </script>
@@ -241,7 +336,16 @@ export default {
 <style scoped>
 .warpper {
   text-align: center;
-  margin-top: 40px;
+  margin-top: 10px;
+}
+.game-model {
+  margin-top: 10rem;
+  text-align: center;
+}
+.model-button {
+  width: 40%;
+  height: 6rem;
+  /* background-color: #5694fb; */
 }
 
 .gobang {
